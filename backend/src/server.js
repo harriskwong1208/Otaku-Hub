f9c95b13f42ede1bea68f94d1deaa633365cf069
Harris Kwong <harriskwong120898@gmail.com>
2023-12-31 15:55:18 -0500

    Added test requesting methods inside server.js (post and get)

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,9 +6,16 @@ const app = express();
 //json payload, it is going to parse that and automatically make that 
 //available to us on request.body
 app.use(express.json());
+
+//Testing post endpoint
 app.post('/hello',(req,res)=>{
-    console.log(req.body);
-    res.send('Hello!');
+    res.send(`Hello ${req.body.name}!!`);
+})
+
+//Testing get endpoint and URL parameter
+app.get('/hello/:name',(req,res)=>{
+    const {name} = req.params;
+    res.send(`Hello ${name} !!`);
 })
 
 app.listen(8000,()=>{
