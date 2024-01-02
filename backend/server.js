452b1d06d7e082a1c286b087846b6b1917cb2b7e
Harris Kwong <harriskwong120898@gmail.com>
2024-01-02 15:25:53 -0500

    Created model, controller, and routes folder. Tested get user method with express server

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,16 @@
 const express = require("express");
 require("dotenv").config(); 
 
-
+const router = require("./routes/user-routes");
 const mongoose = require("mongoose");
 const app = express();
 
+
+
+app.use("/users",router);
+
+
 mongoose.connect(process.env.MONGOURL
-).then(()=>console.log('Successfully connected to database.')
-).catch((e)=>console.log(e));
-
-const db = mongoose.connection;
-
-
-
-
-
-
-app.get('/',(req,res)=>{
-    res.send("Hello again")
-});
-
-app.listen(5000,()=>console.log("Connected and listening on port 5000"));
+    ).then(()=>app.listen(5000,()=>
+        console.log("Connected and listening on port 5000"))
+    ).catch((e)=>console.log(e));
