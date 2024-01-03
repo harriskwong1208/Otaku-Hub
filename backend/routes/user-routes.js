d59c25db065446e8586b4457f0564d10588be0a8
Harris Kwong <harriskwong120898@gmail.com>
2024-01-03 15:05:44 -0500

    Added get,post,delete,and put http methods. Connected to otakuhub cloud database.

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,10 +1,13 @@
 const express = require("express");
-const {getAllUsers} = require("../controller/user-controller");
+const {getAllUsers,addUser,updateUser,deleteUser,getUser} = require("../controller/user-controller");
 
 
 const router = express.Router();
 
 router.get("/",getAllUsers);
-
+router.post("/",addUser);
+router.put("/:id",updateUser);
+router.delete("/:id",deleteUser);
+router.get("/:id",getUser);
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,19 @@ const router = require("./routes/user-routes");
 const mongoose = require("mongoose");
 const app = express();
 
+//middleware function to convert json string to json object 
+app.use(express.json());
+
 
 
 app.use("/users",router);
 
 
-mongoose.connect(process.env.MONGOURL
-    ).then(()=>app.listen(5000,()=>
-        console.log("Connected and listening on port 5000"))
-    ).catch((e)=>console.log(e));
+
+
+mongoose.connect(process.env.MONGOURL, {
+      dbName: 'OtakuHub',
+    }).then(()=>{
+        app.listen(5000,()=>
+            console.log("Connected and listening on port 5000,connected to the database."))
+    }).catch(e=>console.log(e));
