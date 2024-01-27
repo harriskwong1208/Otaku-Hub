a9356de35bab750e6369d241c9b3908f15f095ff
Harris Kwong <harriskwong120898@gmail.com>
2024-01-27 16:22:57 -0500

    Hosted express rest api on google cloud, changed endpoints axios fetches to hosted api.

--- src/Collections/Users.js
+++ src/Collections/Users.js
@@ -3,21 +3,25 @@ import { AuthContext } from "../components/AuthContext";
 import { getCurrentUser } from "../auth";
 
 export  async function getUserIdByEmail(email){
-    let users = await axios.get('http://localhost:5000/users');
+    // let users = await axios.get('http://localhost:5000/users');
+    let users = await axios.get('https://otakuhubexpress.ue.r.appspot.com/api/users/');
+
     users = users.data.users;
     for(let i =0;i < users.length;i++){
         if(users[i].email == email){
             return users[i];
         }
     }
-    return Error("Cannot find user email in database");
+    return new Error("Cannot find user email in database");
 }
 
 export  async function addUserSubId(subId,id){
-    await axios.put(`http://localhost:5000/users/${id}`,{
+    // await axios.put(`http://localhost:5000/users/${id}`,{
+    //     subId: subId
+    // });
+    await axios.put(`https://otakuhubexpress.ue.r.appspot.com/api/users/${id}`,{
         subId: subId
     });
-
 }
 
 export  async function getUserFromCognito(){
@@ -26,7 +30,7 @@ export  async function getUserFromCognito(){
 }
 //Returns 1 if email exists in database, -1 otherwise
 export  async function findEmail(email){
-    let users = await axios.get('http://localhost:5000/users');
+    let users = await axios.get('https://otakuhubexpress.ue.r.appspot.com/api/users/');
     users = users.data.users;
     for(let i =0; i < users.length;i++){
         if(users[i].email == email){
