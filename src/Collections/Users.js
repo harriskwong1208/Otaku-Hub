bf8443eacd6131e1e5b899648e1d3d9957c22eef
Harris Kwong <harriskwong120898@gmail.com>
2024-02-28 17:00:18 -0500

    (collection/ Manga) -  Implemented manga collection

--- src/Collections/Users.js
+++ src/Collections/Users.js
@@ -159,3 +159,20 @@ export async function getCurrentUserDatabase(){
     }
     return new Error("Error in getting user.")
 }
+
+
+//Check if manga is already in user's manga list
+//RETURNS TRUE if manga already in manga list
+export async function checkUserMangaList(id,mangaId){
+    try{
+        const response = await axios.get(apiEndPoints.localHost+`users/${id}`);
+        const user = response.data.user;
+        const found = user.mangaList.find(u => u == mangaId);
+        if(found){
+            return true;
+        }
+    }catch(e){
+        console.log(e);
+    }
+    return false;
+}
