dcc357377098318c4c3e3117990afd9e859b0aed
harris <harriskwong120898@gmail.com>
2024-04-07 16:15:48 -0400

    Error page styling in progress

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,15 +1,9 @@
 import "../styles/Error.css";
-export function Error(props) {
+export function Error() {
 
-  if (props.type == "NotLoggedIn") {
-    return (
-    <div className={props.type}>
-      <h1>
-        Invalid Access !!!
-      </h1>
-      <div className="btnContainer">
-        <button ><a href="/login">Sign In Here!</a></button>
-      </div>
-    </div>)
-  }
+  return (
+    <div className="ErrorPage">
+      
+    </div>
+  )
 }
