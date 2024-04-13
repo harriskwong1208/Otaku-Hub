92400bec34f8f9b979a42c126df7850bdb9c8a49
harris <harriskwong120898@gmail.com>
2024-04-12 21:27:42 -0400

    Welcome page styling in progress

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -8,6 +8,7 @@ import { getUserWatchList, getUserMangaList } from "../Collections/Users";
 import { useNavigate } from "react-router-dom";
 import { getAnime } from "../Collections/Anime";
 import { getManga } from "../Collections/Manga";
+import background from "../static/homepage.png"
 import axios from "axios";
 function HomePage() {
   document.body.style = "background: #10131f;";
@@ -170,25 +171,11 @@ function HomePage() {
 
 function WelcomePage() {
   return (
-    <header>
-      <div className="welcome-message">
-        Welcome to OtakuHub, the ultimate destination for all your anime and
-        manga tracking needs! Whether you're a seasoned otaku or just starting
-        your journey into the world of anime and manga, this platform is
-        designed to enhance your experience and connect you with fellow
-        enthusiasts. With OtakuHub, you can discover new anime and manga titles,
-        add them to your personalized watch list, and keep track of your
-        progress. Connect with friends, share recommendations, and explore their
-        watch lists to find your next favorite series.
-      </div>
-      <div className="animation">
-        <img src={frieren}></img>
-      </div>
-      <div className="sign-in">
-        <a href="/signup">Sign up</a>&nbsp;or&nbsp;
-        <a href="/login">Log in</a>&nbsp;here!
-      </div>
-    </header>
+    <div id="welcomePage">
+        <div className="title">
+          OtakuHub
+        </div>
+    </div>
   );
 }
 
