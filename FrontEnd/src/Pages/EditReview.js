5569668025b5826bdee639ca5c4f8174179e9910
Harris Kwong <harriskwong120898@gmail.com>
2024-07-27 14:18:11 -0400

    Loaded review info into edit Review page

--- FrontEnd/src/Pages/EditReview.js
+++ FrontEnd/src/Pages/EditReview.js
@@ -9,17 +9,21 @@ import Error from "../components/Error";
 export function EditReview() {
   const { id } = useParams();
   const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState();
   const [reviewDetails, setReviewDetails] = useState();
   const ratingScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const [title, setTitle] = useState();
+  const [description, setDescription] = useState();
+  const [rating, setRating] = useState();
 
   async function loadReview() {
-    setLoading(true);
     try {
       const review = await axios.get(apiEndPoints.backEndApi + `review/${id}`);
       if (review) {
-        console.log(review?.data.review);
         setReviewDetails(review?.data.review);
+        setTitle(review?.data.review.title);
+        setDescription(review?.data.review.description);
+        setRating(review?.data.review.rating);
       }
     } catch (e) {
       console.log(e);
@@ -29,7 +33,12 @@ export function EditReview() {
   }
 
   useEffect(() => {
-    loadReview().then(setLoading(false)).catch(setLoading(false));
+    setLoading(true);
+    loadReview()
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(setLoading(false));
   }, []);
 
   if (error) {
@@ -44,35 +53,44 @@ export function EditReview() {
       <div className="reviewContainer">
         <label for="reviewRating">Rating: </label>
         <select
-          // onChange={(e) => {
-          //   setReviewRating(e.target.value);
-          // }}
+          onChange={(e) => {
+            setRating(e.target.value);
+          }}
           name="reviewRating"
           id="reviewRating"
         >
           {ratingScale.map((num, index) => {
+            if (num == rating) {
+              return (
+                <option selected value={num}>
+                  {num}
+                </option>
+              );
+            }
             return <option value={num}>{num}</option>;
           })}
         </select>
-        <label for="addReviewTitle"></label>
+        <label for="ReviewTitle"></label>
         <input
-          // onChange={(e) => {
-          //   setReviewTitle(e.target.value);
-          // }}
-          id="addReviewTitle"
-          name="addReviewTitle"
+          onChange={(e) => {
+            setTitle(e.target.value);
+          }}
+          id="ReviewTitle"
+          name="ReviewTitle"
           placeholder="Enter a title"
           required
+          value={title}
         ></input>
         <label for="reviewText"></label>
         <textarea
-          // onChange={(e) => {
-          //   setReviewDescription(e.target.value);
-          // }}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
           required
           id="reviewText"
           name="reviewText"
           placeholder="Enter Review Here."
+          value={description}
         ></textarea>
         <div className="reviewButtons">
           <button
--- FrontEnd/src/components/Error.js
+++ FrontEnd/src/components/Error.js
@@ -1,21 +1,32 @@
 import "../styles/Error.css";
-import {useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-export default function Error() {
+export default function Error(props) {
   const navigate = useNavigate("");
+
+  if (props.code == 401) {
+    return (
+      <div className="ErrorComp">
+        <div className="Container">
+          <div id="Code">401</div>
+          <div id="Message">Invalid Access</div>
+          <div className="btnContainer">
+            <button onClick={() => navigate("/")}>Back To Home</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="ErrorComp">
       <div className="Container">
-        <div id="Code">
-          404
-        </div>
-        <div id="Message">
-          Sorry, We couldn't find the page.
-        </div>
+        <div id="Code">404</div>
+        <div id="Message">Sorry, We couldn't find the page.</div>
         <div className="btnContainer">
-          <button onClick={()=>navigate("/")}>Back To Home</button>
+          <button onClick={() => navigate("/")}>Back To Home</button>
         </div>
       </div>
     </div>
-  )
+  );
 }
